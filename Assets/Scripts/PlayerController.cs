d65a17e56aac287e3ab972a2d4b394c8216c133c
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-12-22 11:21:13 -0300

    Add shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float cooldownAttack = 2f;
+    [SerializeField] private GameObject projectilePrefab;
 
     private PlayerInput playerInput;
     private Rigidbody2D rgbody2D;
@@ -22,11 +23,12 @@ public class PlayerController : MonoBehaviour
     private InputAction attack;
     private Vector2 movement;
     private PlayerStates currentState;
-    private Vector2 lastMovement;
+    private Vector2 lastMovement = new Vector2(0f, 1f);
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
+
         rgbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
 
@@ -38,6 +40,20 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        bool isAttacking = attack.ReadValue<float>() == 1;
+        if (isAttacking && currentState != PlayerStates.Attack)
+        {
+            StartCoroutine(AttackCoroutine());
+        }
+        else if (currentState == PlayerStates.Walk)
+        {
+            movement = move.ReadValue<Vector2>();
+
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+        }
+
         if (movement != Vector2.zero)
         {
             lastMovement = movement;
@@ -47,31 +63,61 @@ public class PlayerController : MonoBehaviour
             animator.SetFloat("Last Move Horizontal", lastMovement.x);
             animator.SetFloat("Last Move Vertical", lastMovement.y);
         }
-
-        bool isAttacking = attack.ReadValue<float>() == 1;
-        if (isAttacking && currentState != PlayerStates.Attack)
-        {
-            StartCoroutine(AttackCoroutine());
-        }
-        else if (currentState == PlayerStates.Walk)
-        {
-            movement = move.ReadValue<Vector2>();
-            animator.SetFloat("Horizontal", movement.x);
-            animator.SetFloat("Vertical", movement.y);
-            animator.SetFloat("Speed", movement.sqrMagnitude);
-        }
     }
 
     private IEnumerator AttackCoroutine()
     {
+        currentState = PlayerStates.Attack;
+
         animator.SetBool("Attack", true);
         animator.SetFloat("Last Move Horizontal", lastMovement.x);
         animator.SetFloat("Last Move Vertical", lastMovement.y);
+
+        Shoot();
         yield return null;
 
         animator.SetBool("Attack", false);
         yield return new WaitForSeconds(0.33f);
+
+        currentState = PlayerStates.Walk;
     }
+
+    private void Shoot()
+    {
+        var projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        Vector3 projectileRotation = Vector3.zero;
+        Vector2 direction = Vector2.zero;
+
+        if (lastMovement.x == 1)
+        {
+            direction = Vector2.right;
+            projectileRotation = new Vector3(0f, 0f, -90f);
+        }
+        else if (lastMovement.y == 1)
+        {
+            projectileRotation = new Vector3(0f, 0f, 0f);
+            direction = Vector2.up;
+        }
+        else if (lastMovement.x == -1)
+        {
+            projectileRotation = new Vector3(0f, 0f, 90f);
+            direction = Vector2.left;
+        }
+        else if (lastMovement.y == -1)
+        {
+            projectileRotation = new Vector3(0f, 0f, -180f);
+            direction = Vector2.down;
+        }
+        else
+        {
+            projectileRotation = new Vector3(0f, 0f, 0f);
+            direction = Vector2.up;
+        }
+
+        projectile.GetComponent<Rigidbody2D>().velocity += direction * 10;
+        projectile.transform.Rotate(projectileRotation);
+    }
+
 
     private void FixedUpdate()
     {
